32c7d314853ed4108523f3aa6c815f26527157f2
jannkasper <jann.kasper@gmail.com>
2020-11-06 07:47:04 +0100

    missed files

---
+++ server/routes/auth.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userController = require('../controllers/user.controller');
+
+router.get('/all', userController.getAll);
+
+// Register a new User
+router.post('/register', userController.register);
+
+// Login
+router.post('/login', userController.login);
+
+module.exports = router;
